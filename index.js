78a9c2bc1892d066a5ed620bc253070131563156
Vibhav Sinha <vibhavsinha91@gmail.com>
2017-09-24 14:17:19 +0530

    breaking: error tracking and always json

--- index.js
+++ index.js
@@ -1,30 +1,54 @@
+const randomString = () => Math.random().toString(36).substring(2, 15) +
+  Math.random().toString(36).substring(2, 15);
 
+/**
+ * An errror class which handles HTTP status codes.
+ */
 exports.HTTPError = class HTTPError extends Error {
   /**
+   * return an instance of HTTPError which can be handled to provide correct
+   * status code along with error message. Check the default function for more
+   * details
+   *
    * @param {integer} code
    * @param {any} obj
    */
   constructor(code, obj) {
     super(obj);
     this.code = code;
+    this.message = (typeof obj === 'object') ? obj : {
+      message: obj.toString(),
+    };
   }
 };
 
+/**
+ * convert a promise based function into an express middleware.
+ * the returned function is not exactly a middleware because it does not hanlle
+ * any next function.
+ * @param {function(req:object):any} promiseFunction
+ * @return {function(req:object, res:object):void}
+ */
 exports.default = (promiseFunction) => {
   return (req, res) => {
     new Promise((resolve, reject) => {
       resolve(promiseFunction(req));
     })
       .catch((e) => {
-        console.log('Error caught: ', e);
-        res.status(e.code || 500);
-        return e.message || 'Internal server error';
+        if (e.constructor.name === 'HTTPError') {
+          res.status(e.code);
+          return e.message;
+        }
+        let rnd = randomString();
+        console.log(`Error caught ${rnd}: `, e);
+        res.status(500);
+        return {
+          message: `Internal server error. Mention this error to our support: ${rnd}`,
+        };
       })
       .then((data) => {
         if (typeof data === 'object') {
           return res.json(data);
-        } else if (typeof data === 'string') {
-          return res.send(data);
         }
         res.send(data.toString());
       });
